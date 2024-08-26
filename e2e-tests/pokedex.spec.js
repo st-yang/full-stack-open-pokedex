e2384e8f4a18a6845f14bb6988f527f8c0b8fbcb
YANG Shengtao <eranikus424@gmail.com>
2024-08-26 16:20:41 +0800

    11.9 Simple end-to-end tests

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,17 @@
+const { test, describe, expect } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('page of a particular Pokemon can be opened', async ({ page }) => {
+    await page.goto('/')
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('overgrow')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
