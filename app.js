2ff6795c293b7f19b06ca932de859f0af500d1df
YANG Shengtao <eranikus424@gmail.com>
2024-08-26 17:38:31 +0800

    Revert "11.12 Health check - ensure a broken version does not get deployed"
    This reverts commit c34a10b1b416b0d523bd8e47586fd238973ea7b8.

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-// eslint-disable-next-line no-unused-vars
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  '
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
